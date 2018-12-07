ddaeea7758ec74b18bfa20e3cc618b0c0bbff429
2013111015 <43342114+2013111015@users.noreply.github.com>
2018-12-07 15:05:59 +0100

    three

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,30 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    connect(&first,SIGNAL(dataReadyRead(QByteArray)),this,SLOT(database(QByteArray)));
+//MainWindow::MainWindow(QWidget *parent) :
+ //   QMainWindow(parent),
+ //   ui(new Ui::MainWindow)
+//{
+//    ui->setupUi(this);
+//    MainWindow::makePlot();
+//}
 
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
-void MainWindow::database(QByteArray data)
-{
-QString dataString;
-dataString = data;
-ui->pushButton->setText(dataString);
-
-
-}
-
-void MainWindow::on_pushButton_clicked()
-{
-    first.makeRequest("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=15min&outputsize=full&apikey=demo");
-}
+//MainWindow::~MainWindow()
+//{
+  //  delete ui;
+//}
+//void MainWindow::makePlot()
+//{
+//    QCPBars *myBars = new QCPBars(ui->customPlot->xAxis, ui->customPlot->yAxis);
+    // now we can modify properties of myBars:
+ //   myBars->setName("Bars Series 1");
+ //   QVector<double> keyData;
+ //   QVector<double> valueData;
+ //   keyData << 1 << 2 << 3;
+ //   valueData << 2 << 4 << 8;
+ //   myBars->setData(keyData, valueData);
+ //   ui->customPlot->rescaleAxes();
+ //   ui->customPlot->replot();
+//}
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include "networkclass.h"
+
 namespace Ui {
 class MainWindow;
 }
@@ -14,16 +14,11 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
 private slots:
-    void database(QByteArray data);
-
-
-    void on_pushButton_clicked();
-
+    void makePlot();
 private:
     Ui::MainWindow *ui;
-    NetworkClass first;
-
 };
 
 #endif // MAINWINDOW_H
